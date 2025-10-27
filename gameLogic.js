092558bbfc5734b23b38df4f966461867576b140
vararatc-cpu <vararatc@unemi.edu.ec>
2025-10-26 22:03:56 -0500

    se agrego un sistema para cambiar la clave musical entre Sol, Fa, Do en tercera linea y Do en cuarta linea mediante un boton que cicla entre las opciones.

--- gameLogic.js
+++ gameLogic.js
@@ -1,12 +1,12 @@
 var clave = "F"; //G F C3raLinea C4taLinea
-var actual8va= 4;
+var actual8va = 4;
 
 function eligio(btn) {
   cambiaColor(btn);
   var notaActualNombre = notas["nota" + notaActual].nombre;
-  console.log(notaActualNombre , btn.innerHTML);
+  console.log(notaActualNombre, btn.innerHTML);
   if (btn.innerText === notaActualNombre) {
-    playNota(notaActualNombre + notas["nota" + notaActual].octava); 
+    playNota(notaActualNombre + notas["nota" + notaActual].octava);
     cambioColorNota("var(--verde)");
   } else {
     playNota(btn.innerText + "4");
@@ -26,47 +26,47 @@ function eligio(btn) {
 }
 
 // Función para obtener la notación musical según la posición
-function notacionAm (posicion) {
+function notacionAm(posicion) {
   if (clave == "G") {
     switch (posicion) {
       case -46:
-        actual8va= "5";
-        return "A";        
+        actual8va = "5";
+        return "A";
       case -29:
-        actual8va= "5";
+        actual8va = "5";
         return "G";
       case -12:
-        actual8va= "5";
+        actual8va = "5";
         return "F";
       case 5:
-        actual8va= "5";
+        actual8va = "5";
         return "E";
       case 22:
-        actual8va= "5";
+        actual8va = "5";
         return "D";
       case 39:
-        actual8va= "5";
+        actual8va = "5";
         return "C";
       case 57:
-        actual8va= "4";
+        actual8va = "4";
         return "B";
       case 74:
-        actual8va= "4";
+        actual8va = "4";
         return "A";
       case 93:
-        actual8va= "4";
+        actual8va = "4";
         return "G";
       case 109:
-        actual8va= "4";
+        actual8va = "4";
         return "F";
       case 127:
-        actual8va= "4";
+        actual8va = "4";
         return "E";
       case 145:
-        actual8va= "4";
+        actual8va = "4";
         return "D";
       case 162:
-        actual8va= "4";
+        actual8va = "4";
         return "C";
       default:
         return "Desconocida";
@@ -74,143 +74,155 @@ function notacionAm (posicion) {
   } else if (clave == "F") {
     switch (posicion) {
       case -46:
-        actual8va= "4"; 
+        actual8va = "4";
         return "C";
       case -29:
-        actual8va= "3";
+        actual8va = "3";
         return "B";
       case -12:
-        actual8va= "3";
+        actual8va = "3";
         return "A";
       case 5:
-        actual8va= "3";
-        return "G"; 
+        actual8va = "3";
+        return "G";
       case 22:
-        actual8va= "3";
+        actual8va = "3";
         return "F";
       case 39:
-        actual8va= "3";
+        actual8va = "3";
         return "E";
       case 57:
-        actual8va= "3";
+        actual8va = "3";
         return "D";
       case 74:
-        actual8va= "3";
+        actual8va = "3";
         return "C";
       case 93:
-        actual8va= "2";
+        actual8va = "2";
         return "B";
       case 109:
-        actual8va= "2";
+        actual8va = "2";
         return "A";
       case 127:
-        actual8va= "2";
+        actual8va = "2";
         return "G";
       case 145:
-        actual8va= "2";
+        actual8va = "2";
         return "F";
       case 162:
-        actual8va= "2";
+        actual8va = "2";
         return "E";
       default:
         return "Desconocida";
     }
-  } else if (clave == "C3raLinea") { 
+  } else if (clave == "C3raLinea") {
     switch (posicion) {
       case -46:
-        actual8va= "4";
+        actual8va = "4";
         return "B";
       case -29:
-        actual8va= "4";
+        actual8va = "4";
         return "A";
       case -12:
-        actual8va= "4";
+        actual8va = "4";
         return "G";
       case 5:
-        actual8va= "4";
+        actual8va = "4";
         return "F";
       case 22:
-        actual8va= "4";
+        actual8va = "4";
         return "E";
       case 39:
-        actual8va= "4";
+        actual8va = "4";
         return "D";
       case 57:
-        actual8va= "4";
+        actual8va = "4";
         return "C";
       case 74:
-        actual8va= "3";
+        actual8va = "3";
         return "B";
       case 93:
-        actual8va= "3";
+        actual8va = "3";
         return "A";
       case 109:
-        actual8va= "3";
+        actual8va = "3";
         return "G";
       case 127:
-        actual8va= "3";
+        actual8va = "3";
         return "F";
       case 145:
-        actual8va= "3";
+        actual8va = "3";
         return "E";
       case 162:
-        actual8va= "3";
+        actual8va = "3";
         return "D";
+      default:
+        return "Desconocida";
+    }
+  } else if (clave == "C4taLinea") {
+    switch (posicion) {
+      case -46:
+        actual8va = "4";
+        return "G";
+      case -29:
+        actual8va = "4";
+        return "F";
+      case -12:
+        actual8va = "4";
+        return "E";
+      case 5:
+        actual8va = "4";
+        return "D";
+      case 22:
+        actual8va = "4";
+        return "C";
+      case 39:
+        actual8va = "3";
+        return "B";
+      case 57:
+        actual8va = "3";
+        return "A";
+      case 74:
+        actual8va = "3";
+        return "G";
+      case 93:
+        actual8va = "3";
+        return "F";
+      case 109:
+        actual8va = "3";
+        return "E";
+      case 127:
+        actual8va = "3";
+        return "D";
+      case 145:
+        actual8va = "3";
+        return "C";
+      case 162:
+        actual8va = "2";
+        return "B";
       default:
         return "Desconocida";
     }
   }
-  else if (clave == "C4taLinea") {
-    switch (posicion) {
-      case -46:
-        actual8va= "4";
-        return "G";
-      case -29:
-        actual8va= "4";
-        return "F";
-      case -12:
-        actual8va= "4";
-        return "E";
-      case 5:
-        actual8va= "4";
-        return "D";
-      case 22:
-        actual8va= "4";
-        return "C";
-      case 39:
-        actual8va= "3";
-        return "B";
-      case 57:
-        actual8va= "3";
-        return "A";
-      case 74:
-        actual8va= "3";
-        return "G";
-      case 93:
-        actual8va= "3";
-        return "F";
-      case 109:
-        actual8va= "3";
-        return "E";
-      case 127:
-        actual8va= "3";
-        return "D";
-      case 145:
-        actual8va= "3";
-        return "C";
-      case 162:
-        actual8va= "2";
-        return "B";
-      default:
-        return "Desconocida";
-    }
-  }
-};
+}
 
-function cambiaClave() {
-  var selectClave = document.getElementById("selectClave");
-  clave = selectClave.value;
-  console.log("Clave cambiada a: " + clave);
+// Cambiar clave musical
+var x = 0;
+function cambiarClave() {
+  
+  if (x == 0) {
+    clave = "G";
+  } else if (x == 1) {
+    clave = "F";
+  } else if (x == 2) {
+    clave = "C3raLinea";
+  } else if (x == 3) {
+    clave = "C4taLinea";
+  }
+  x++;
+  if (x > 3) {
+    x = 0;
+  }
   limpiarNotas();
 }
 
